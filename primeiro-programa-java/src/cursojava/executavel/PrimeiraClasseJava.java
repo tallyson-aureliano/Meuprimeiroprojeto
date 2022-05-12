3e03ec916feb1f7c96f540a3ee6e4d1b66b7864a
conta <conta@noteDoTallyson>
2022-05-12 15:36:17 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,15 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*Main é um método autoexecutável em Java*/
+	public static void main(String[] args) {
+		
+		/*escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+		
+
+	}
+
+}
+	
+	
